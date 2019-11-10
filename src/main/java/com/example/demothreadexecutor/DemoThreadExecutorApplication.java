88db41a2370194e281103ca4fa0272e1ac00789f
titom <tito.mathews@gmail.com>
2019-11-09 19:56:15 -0700

    beta 0.01

---
+++ src/main/java/com/example/demothreadexecutor/DemoThreadExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demothreadexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThreadExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoThreadExecutorApplication.class, args);
+	}
+
+}
